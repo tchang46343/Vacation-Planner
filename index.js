44d1047084cf266882bd01fd298cca5a2d851e8e
Terrance Chang <tchang46343@gmail.com>
2019-06-30 08:08:01 -0700

    upload vacation planner to github first submission

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const apiKey = 'api_key=IFG9lzVqlJVIrnLUSBtJOFKPe86a3iR5942IpNAP';
+
+function watchForm(){
+    $('form').submit(event =>{
+        event.preventDefault();
+        console.log("😭");
+        const searchTerm = $('.stateSearch').val();
+        const maxResults = $('.parkQty').val();
+    getNationalParks(searchTerm, maxResults);
+    });
+}
+
+function getNationalParks(searchTerm,maxResults){
+    $('#js-searchParks').empty();
+    const baseurl = 'https://developer.nps.gov/api/v1/parks';
+    const userState = 'stateCode='+ searchTerm;
+    const resultsQty = 'limit=' + maxResults;
+    const url = baseurl + '?' + userState + '&' + resultsQty+ '&' + apiKey; 
+      console.log(url);
+      fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      throw new Error();
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('.error-message').text(`Something went wrong.Try again later.`);
+      });
+}
+
+function displayResults(responseJson){
+  console.log(responseJson);
+  $('#result-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++){
+    $('#result-list').append(
+      `<h3>Article Title: ${responseJson.data[i].fullName}</h3>
+        <ul>
+          <h4>Description:</h4>
+          <li>${responseJson.data[i].description}</li>
+          <h4>For More info visit:</h4>
+          <li>${responseJson.data[i].url}</li>
+        </ul>`
+      )};
+  $('#results').removeClass('hidden');
+
+}
+$(watchForm);
